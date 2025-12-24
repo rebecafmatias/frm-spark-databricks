0456c3a5900bb513d933e8321d0412ce2ccaef8d
rebecafmatias <rebecafeitosamatias@gmail.com>
2025-12-23 21:31:26 -0300

    starting mod-2-pr-2.py and testing spark session and spark context

--- src/app/mod-2-pr-2.py
+++ src/app/mod-2-pr-2.py
@@ -1,22 +1,27 @@
 """
-docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+docker exec -it spark-master /opt/spark/bin/spark-submit \
   --master spark://spark-master:7077 \
   --deploy-mode client \
-  /opt/bitnami/spark/jobs/app/mod-2-pr-2.py
+  /opt/spark/jobs/app/mod-2-pr-2.py
 """
 
 from pyspark.sql import SparkSession
 
 # TODO 1 = create spark session
+
 spark = SparkSession.builder \
-    .config("spark.executor.memory", "512m") \
-    .config("spark.driver.memory", "1g") \
     .getOrCreate()
 
 # TODO 2 = create spark context
+
 sc = spark.sparkContext
+
+print(f"\n###########")
+print(f"\nSpark Version: {spark.version}")
+print(f"Application ID: {sc.applicationId}")
+print(f"Available cores: {sc.defaultParallelism}")
+print(f"\n###########\n")
 
 # TODO 3 = the spark code here
 
 # TODO 4 = stop the spark context
-spark.stop()
