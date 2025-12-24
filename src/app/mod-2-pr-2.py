c85a2eab3dfb96dc9a5fda07065fe7627bdc54b2
rebecafmatias <rebecafeitosamatias@gmail.com>
2025-12-24 17:34:01 -0300

    updating py file to inform sparksession configs

--- src/app/mod-2-pr-2.py
+++ src/app/mod-2-pr-2.py
@@ -2,6 +2,10 @@
 docker exec -it spark-master /opt/spark/bin/spark-submit \
   --master spark://spark-master:7077 \
   --deploy-mode client \
+  --driver-memory 1g \
+  --executor-memory 2g \
+  --executor-cores 2 \
+  --total-executor-cores 4 \
   /opt/spark/jobs/app/mod-2-pr-2.py
 """
 
@@ -10,10 +14,11 @@ from pyspark.sql import SparkSession
 # TODO 1 = create spark session
 
 spark = SparkSession.builder \
-    .appName("testingSpark") \
-    .config("spark.executer.memory","512m") \
-    .config("spark.driver.memory","1g") \
     .getOrCreate()
+    # .appName("testingSpark") \
+    # .config("spark.executer.memory","512m") \
+    # .config("spark.driver.memory","1g") \
+    # .getOrCreate()
 
 # TODO 2 = create spark context
 
@@ -23,6 +28,11 @@ print(f"\n###########\n")
 print(f"Spark Version: {spark.version}")
 print(f"Application ID: {sc.applicationId}")
 print(f"Available cores: {sc.defaultParallelism}")
+print(f"\n###########\n")
+
+for item in sorted(sc.getConf().getAll()):
+    print(f"{item[0]}: {item[1]}")
+
 print(f"\n###########\n")
 
 # TODO 3 = the spark code here
