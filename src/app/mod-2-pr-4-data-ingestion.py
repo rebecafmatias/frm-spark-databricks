fc46a5dc82212d58e7af8c3d572b7b95252a5b8c
rebecafmatias <rebecafeitosamatias@gmail.com>
2025-12-27 19:02:54 -0300

    counting rows

--- src/app/mod-2-pr-4-data-ingestion.py
+++ src/app/mod-2-pr-4-data-ingestion.py
@@ -6,19 +6,38 @@ docker exec -it spark-master /opt/spark/bin/spark-submit \
 """
 
 from pyspark.sql import SparkSession
+import pyspark.sql.types as t
 
 spark = SparkSession.builder \
     .getOrCreate()
 
-path_restaurants = "/opt/spark/storage/mysql/restaurants/01JS4W5A7YWTYRQKDA7F7N95VY.jsonl"
+path_restaurants = "/opt/spark/storage/mysql/restaurants/*.jsonl"
+
+restaurant_schema = t.StructType([
+    t.StructField("address", t.StringType(), True),
+    t.StructField("average_rating", t.DoubleType(), True),
+    t.StructField("city", t.StringType(), True),
+    t.StructField("closing_time", t.StringType(), True),
+    t.StructField("cnpj", t.StringType(), True),
+    t.StructField("country", t.StringType(), True),
+    t.StructField("cuisine_type", t.StringType(), True),
+    t.StructField("dt_current_timestamp", t.StringType(), True),
+    t.StructField("name", t.StringType(), True),
+    t.StructField("num_reviews", t.IntegerType(), True),
+    t.StructField("opening_time", t.StringType(), True),
+    t.StructField("phone_number", t.StringType(), True),
+    t.StructField("restaurant_id", t.IntegerType(), True),
+    t.StructField("uuid", t.StringType(), True)
+])
 
 df_restaurants = spark.read \
-    .option("multiline", "true") \
-    .option("mode", "PERMISSIVE") \
+    .schema(restaurant_schema) \
     .json(path_restaurants)
 
+# qty_restaurant_rows = df_restaurants.count()
+
 print(f"\n#########\n")
-df_restaurants.show(5)
+print(f"Quantidade de linhas: {df_restaurants.count()}")
 print(f"\n#########\n")
 df_restaurants.printSchema()
 print(f"\n#########\n")
