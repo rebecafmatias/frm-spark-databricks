c3b2f27e4922e31a7b64564e8fae92c8d4c21415
rebecafmatias <rebecafeitosamatias@gmail.com>
2025-12-30 23:07:15 -0300

    adjusting spark reading

--- src/app/mod-2-pr-6-complex-transformation.py
+++ src/app/mod-2-pr-6-complex-transformation.py
@@ -9,9 +9,13 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .getOrCreate()
 
-restaurants_path = "/opt/spark/storage/mysql/restaurants/01JS4W5A7YWTYRQKDA7F7N95VY.jsonl"
-drivers_path = "/opt/spark/storage/postgres/drivers/01JS4W5A74BK7P4BPTJV1D3MHA.jsonl"
-orders_path = "/opt/spark/storage/kafka/orders/01JS4W5A7XY65S9Z69BY51BEJ4.jsonl"
+restaurants_df = spark.read.json("/opt/spark/storage/mysql/restaurants/01JS4W5A7YWTYRQKDA7F7N95VY.jsonl")
+drivers_df = spark.read.json("/opt/spark/storage/postgres/drivers/01JS4W5A74BK7P4BPTJV1D3MHA.jsonl")
+orders_df = spark.read.json("/opt/spark/storage/kafka/orders/01JS4W5A7XY65S9Z69BY51BEJ4.jsonl")
+
+restaurants_df.printSchema()
+drivers_df.printSchema()
+orders_df.printSchema()
 
 # TODO 1. aggregations and grouping
 
